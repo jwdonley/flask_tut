8b85bbd6d5fae6c0d3eebcdc73d4d44577c1a6a4
Joe Donley <jdonley83@gmail.com>
2021-12-27 16:21:28 -0800

    going through quickstart

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
 from flask import Flask
+from flask import request
+from flask import url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "do_the_login"
+    else:
+        return "show_the_login_form"
+
+@app.route('/user/<username>')
+def profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+with app.test_request_context():
+    print(url_for('hello_world'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
